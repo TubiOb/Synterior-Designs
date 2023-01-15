0aaa63f20f9379464a9a55f487c517433e6ec235
TubiOb <tubiobaloluwa@gmail.com>
2023-01-15 07:19:35 +0100

    Working on the Portfolio Page

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ hamburger.addEventListener('click', () => {
 
     mobile_menu.classList.toggle('is-open');
 })
+
+
+//IMAGE SLIDESHOW
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("image-container");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
