da55badaa8b82b57201158028b17132b50897a92
TubiOb <tubiobaloluwa@gmail.com>
2023-01-19 11:20:22 +0100

    Fixing the portfolio page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 const hamburger = document.querySelector('.hamburger');
 const hamburger_icon = hamburger.querySelector('span');
 const mobile_menu = document.querySelector('.mobile-menu');
@@ -10,62 +11,11 @@ hamburger.addEventListener('click', () => {
 
 
 //IMAGE SLIDESHOW
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//     var i;
-//     var slides = document.getElementsByClassName("image-container");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) { slideIndex = 1 }
-//     slides[slideIndex - 1].style.display = "block";
-//     setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-
-// var slideIndex = 0;
-// var slideGroups = document.getElementsByClassName("image-container");
-// for (var i = 0; i < slideGroups.length; i++) {
-//     showSlides(slideGroups[i]);
-// }
-
-// function showSlides(slideGroups) {
-//     var i;
-//     var slides = slideGroups.getElementsByClassName("mySlides");
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) { slideIndex = 1 }
-//     slides[slideIndex - 1].style.display = "block";
-// }
-
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//     let i;
-//     let slides = document.getElementsByClassName("image-container");
-
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) { slideIndex = 1 }
-
-//     slides[slideIndex - 1].style.display = "block";
-//     setTimeout(showSlides, 6000);
-// }
-
-// 
-
 var slideIndex = 0;
 var slideGroups = document.getElementsByClassName("image-container");
 for (var i = 0; i < slideGroups.length; i++) {
     var slideGroup = slideGroups[i];
-    var slideInterval = slideGroup.getAttribute("data-interval") || 6000; // set default interval to 2s
+    var slideInterval = slideGroup.getAttribute("data-interval") || 6000;
     setInterval(function() {
         showSlides(slideGroup);
     }, slideInterval);
